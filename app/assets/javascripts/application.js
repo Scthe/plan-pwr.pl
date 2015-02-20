a392b676803775e9235a9aa12dc33cef0ad3ba2a
Scthe <marcin1113C@gmail.com>
2015-02-20 19:37:59 +0100

    Remove rails

--- app/assets/javascripts/application.js
+++
@@ -1,9 +0,0 @@
-//= require jquery
-//= require jquery.dataTables.min
-//= require jquery.livequery
-//= require jquery.ui.autocomplete
-//= require rails
-//= require facebox/facebox
-//= require farbtastic/farbtastic
-
-//= require_tree .
